5aef93d22d19c35271d7d374061901d5063382dd
chuck25 <lep.charles@gmail.com>
2025-12-10 19:45:02 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,13 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDTSla_VMUOkXQyTYEQi6yWKCVqY8toRKE",
+  apiKey: "AIzaSyDTSla_VMUOkXQyTYEQi6yWKCVqY8toRKE",
   authDomain: "livre-magog.firebaseapp.com",
   projectId: "livre-magog",
   storageBucket: "livre-magog.firebasestorage.app",
   messagingSenderId: "321783745514",
   appId: "1:321783745514:web:0f040b4b15288908830093",
-  measurementId: "G-2YBD9NJ8Q5"
-};
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+};
+// Initialisation Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
